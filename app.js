2a0cec6ddeceafb2840e74e1b5e74128abb2a75e
Jamie <jamie@lambertstock.com>
2021-03-27 14:55:12 +0000

    Improved naming

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 require("dotenv").config();
 
 // Local Modules
-const { onReady } = require("./modules/onReady");
-const { onMessage } = require("./modules/onMessage");
+const { onDiscordReady } = require("./modules/onDiscordReady");
+const { onDiscordMessage } = require("./modules/onDiscordMessage");
 
 // Discord setup
 const discordClient = new Discord.Client();
@@ -23,11 +23,13 @@ commandFiles.forEach((file) => {
   discordClient.commands.set(command.name, command);
 });
 
-// On "ready" handler ""./modules/onReady"
-discordClient.once("ready", () => onReady(discordClient));
+// On "ready" handler "./modules/onDiscordReady"
+discordClient.once("ready", () => onDiscordReady(discordClient));
 
-// On "message" handler ""./modules/onMessage"
-discordClient.on("message", (message) => onMessage(discordClient, message));
+// On "message" handler "./modules/onDiscordMessage"
+discordClient.on("message", (message) =>
+  onDiscordMessage(discordClient, message)
+);
 
 // Log bot in using the token
 discordClient.login(process.env.DISCORD_TOKEN);
