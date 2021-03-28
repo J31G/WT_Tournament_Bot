db7c9f109f49c16792377bb086b523f06b827c80
Jamie <jamie@lambertstock.com>
2021-03-28 02:17:35 +0100

    ran lint fix

--- modules/onDiscordReady.js
+++ modules/onDiscordReady.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-
 module.exports.onDiscordReady = (discordClient, io) => {
   console.log("The bot is online");
   discordClient.user
@@ -10,10 +8,11 @@ module.exports.onDiscordReady = (discordClient, io) => {
   io.on("connection", (socket) => {
     // New user
     console.log(`Made socket connection id: ${socket.id}`);
+
     // New Team Registrations
     socket.on("new-reg", (data) => {
       // Create message from data
-      
+      // eslint-disable-next-line prettier/prettier
       const newTeam = `__**New Player Sign-Up**__\n\n**Discord**: ${discordClient.users.cache.get(data.discord_user_id)}\n**IGN**: ${data.wt_ign}\n**Language**: ${data.language}`;
 
       // Post message to teams channels on discord
